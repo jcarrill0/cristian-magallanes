affd2298c784a805c34e56be79f8a9b10636fa18
jcarrill0 <josecarrillo8@gmail.com>
2022-01-04 17:05:39 -0600

    added indicator navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,63 @@
 
+addEventListener('DOMContentLoaded', () => {
+  sliderJS();
+  modalJS();
+  navbarSlidingIndicator();
+})
+
+
 // SLIDER JS
-
-let slider = document.querySelector(".slider .slide-track");
-slider.innerHTML += slider.innerHTML;
-
+function sliderJS() {
+  let slider = document.querySelector(".slider .slide-track");
+  slider.innerHTML += slider.innerHTML;
+}
 
 // MODAL JS
+function modalJS() {
+  // Get the modal
+  const modal = document.getElementById("myModal");
 
-// Get the modal
-const modal = document.getElementById("myModal");
+  // Get the button that opens the modal
+  const btnModal = document.getElementById("openModal");
 
-// Get the button that opens the modal
-const btnModal = document.getElementById("openModal");
+  // Get the <span> element that closes the modal
+  const span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+  // When the user clicks the button, open the modal 
+  btnModal.onclick = () => modal.style.display = "block";
 
-// When the user clicks the button, open the modal 
-btnModal.onclick = () => modal.style.display = "block";
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = () => modal.style.display = "none";
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = () => modal.style.display = "none";
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = (e) => {
-  if (e.target == modal) modal.style.display = "none";
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = (e) => {
+    if (e.target == modal) modal.style.display = "none";
+  }
 }
+
+// NAVBAR SLIDING INDICATOR
+function navbarSlidingIndicator() {
+  let section = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('.nav__menu li a');
+  console.log(navLinks)
+
+  window.onscroll = () => {
+      section.forEach(sec => {
+          const top = window.scrollY;
+          const sectionTop = sec.offsetTop - 125;
+          const sectionHeight = sec.clientHeight;
+          let id = sec.getAttribute('id');
+          if(top >= sectionTop && top < sectionTop + sectionHeight) {
+              navLinks.forEach(link => {
+                  link.classList.remove('active');
+                  document.querySelector(`.nav__menu li a[href*=${id}]`).classList.add('active');
+              });
+          }
+      });
+  };
+}
+
+
 
 
 
